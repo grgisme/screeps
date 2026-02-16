fe5dbc3eac173bfb089bf147d2295d1dd7d53041
Garrett Griffin-Morales <garrett.griffin@finite-partners.com>
2026-02-16 14:48:45 -0500

    Deploy v1.0.10

--- src/version.ts
+++ src/version.ts
@@ -1,2 +1,2 @@
-export const SCRIPT_VERSION = "1.0.9";
-export const SCRIPT_SUMMARY = "Overlord Overhaul";
+export const SCRIPT_VERSION = "1.0.10";
+export const SCRIPT_SUMMARY = "Restore default branch";
